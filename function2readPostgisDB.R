1348e8b5b1a149e2162ff84de162ae5d4a2880f3
Ariel Fuentes <ariel.fuentesdi@usach.cl>
2015-09-01 16:44:35 -0300

    Update function2readPostgisDB.R

--- function2readPostgisDB.R
+++ function2readPostgisDB.R
@@ -10,23 +10,21 @@ comuna, ST_AsText(geom) AS wkt_geometry from zonas_estraus"
 geo_db <- function(qsql){
   require(RPostgreSQL)
   con <- dbConnect("PostgreSQL", host = "10.222.128.87", user= "postgres", password="estudios2015..", 
-            dbname = "geo")
+                   dbname = "geo")
   geo_db <- dbGetQuery(con, qsql)
   # dbDisconnect(con)
 }
 shape.df <- as.data.frame(geo_db(qsql))
-for (i in seq(nrow(shape.df))) {
+SPolygons <- function(wkt, p4ss){
   require(rgeos)
-  if (i == 1) {
-    shape.sp = readWKT(shape.df$wkt_geometry[i], shape.df$gid[i])
-  }
-  else {
-    shape.sp = rbind(
-      shape.sp, readWKT(shape.df$wkt_geometry[i], shape.df$gid[i])
-    )
-  }
+  require(sp)
+  list_geom <- lapply(wkt, "readWKT", p4s=p4ss)
+  coords <- mapply(spChFIDs, list_geom, as.character(shape.df[,1]))
+  SPolygonsdf <- SpatialPolygonsDataFrame(SpatialPolygons(unlist(lapply(coords, 
+                                                                 function(x) x@polygons)),
+                                                   proj4string=p4ss), 
+                                   shape.df[, -ncol(shape.df)])
+  SPolygonsdf
 }
 
-shape.polygon <- SpatialPolygonsDataFrame(shape.sp, shape.df[, -ncol(shape.df)])
-shape.line <- SpatialLinesDataFrame(shape.sp, shape.df[, -ncol(shape.df)])
-shape.point <- SpatialPointsDataFrame(shape.sp, shape.df[, -ncol(shape.df)])
+estraus <- SPolygons(wkt = shape.df$wkt_geometry, p4ss = CRS("+proj=longlat +datum=WGS84"))
