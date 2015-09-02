7c99209bc9602777780ae72f9439385e5b4f99a1
Ariel Fuentes <ariel.fuentesdi@usach.cl>
2015-09-02 15:00:40 -0300

    Update function2readPostgisDB.R

--- function2readPostgisDB.R
+++ function2readPostgisDB.R
@@ -1,12 +1,9 @@
 qsql <- "SELECT gid, 
-area,
-area1,
-zona,
-zona_corr,
-macroarea_,
-macroarea,
-comuna_id,
-comuna, ST_AsText(geom) AS wkt_geometry from zonas_estraus"
+ id,
+  area,
+  zona,
+  com,
+  ST_AsText(geom) AS wkt_geometry from centeodlatlon"
 geo_db <- function(qsql){
   require(RPostgreSQL)
   con <- dbConnect("PostgreSQL", host = "10.222.128.87", user= "postgres", password="estudios2015..", 
@@ -21,10 +18,33 @@ SPolygons <- function(wkt, p4ss){
   list_geom <- lapply(wkt, "readWKT", p4s=p4ss)
   coords <- mapply(spChFIDs, list_geom, as.character(shape.df[,1]))
   SPolygonsdf <- SpatialPolygonsDataFrame(SpatialPolygons(unlist(lapply(coords, 
-                                                                 function(x) x@polygons)),
+                                                                        function(x) x@polygons)),
                                                    proj4string=p4ss), 
                                    shape.df[, -ncol(shape.df)])
   SPolygonsdf
 }
 
-estraus <- SPolygons(wkt = shape.df$wkt_geometry, p4ss = CRS("+proj=longlat +datum=WGS84"))
+SLines <- function(wkt, p4ss){
+  require(rgeos)
+  require(sp)
+  list_geom <- lapply(wkt, "readWKT", p4s=p4ss)
+  coords <- mapply(spChFIDs, list_geom, as.character(shape.df[,1]))
+  SLinesdf <- SpatialLinesDataFrame(SpatialLines(unlist(lapply(coords, function(x) x@lines)),
+                                                          proj4string=p4ss), 
+                                          shape.df[, -ncol(shape.df)])
+  SLinesdf
+}
+
+SPoints <- function(wkt, p4ss){
+  require(rgeos)
+  require(sp)
+  list_geom <- lapply(wkt, "readWKT", p4s=p4ss)
+  SPointsdf <- SpatialPointsDataFrame(SpatialPoints(do.call(rbind, lapply(list_geom, data.frame, 
+                                                                          stringsAsFactors=FALSE)),
+                                                    proj4string = p4ss), 
+                                      shape.df[, -ncol(shape.df)])
+  SPointsdf
+}
+polygons.shp <- SPolygons(wkt = shape.df$wkt_geometry, p4ss = CRS("+proj=longlat +datum=WGS84"))
+lines.shp <- SLines(wkt = shape.df$wkt_geometry, p4ss = CRS("+proj=longlat +datum=WGS84"))
+points.shp <- SPoints(wkt = shape.df$wkt_geometry, p4ss = CRS("+proj=longlat +datum=WGS84"))
